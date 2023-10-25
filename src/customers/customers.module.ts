ea8cfb75b60933dd1fd9a3c06557ccbf4704d9d7
Mila Vernazza <milavernazza@gmail.com>
2023-10-24 22:04:42 -0300

    Configured CustomersModule and AppModule

---
+++ src/customers/customers.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CustomersService } from './services/customers.service';
+import { CustomersController } from './controllers/customers.controller';
+
+@Module({
+  providers: [CustomersService],
+  controllers: [CustomersController],
+})
+export class CustomersModule {}
